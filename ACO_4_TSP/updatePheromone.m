aed08e26c6137756cdaf29f46529c05a23f72cf3
martinaspeciale <mspeciale127001@gmail.com>
2025-09-11 10:29:26 +0200

    Add pheromone update: evaporation and deposit on selected/best tours

---
+++ ACO_4_TSP/updatePheromone.m
@@ -0,0 +1,22 @@
+function [tau] = updatePheromone(tau, colony)
+% Update the phromone matrix. 
+
+% nodeNo = length(colony.ant(1).tour);
+antNo = length(colony.ant(:));
+
+for i = 1 : antNo % for each ant
+
+    nodeNo = length(colony.ant(i).tour);
+
+    for j = 1 : nodeNo-1 % for each node in the tour
+
+        currentNode = colony.ant(i).tour(j);
+        nextNode = colony.ant(i).tour(j+1);
+        
+        tau(currentNode, nextNode) = tau(currentNode, nextNode)  + 1./ colony.ant(i).fitness;
+        tau(nextNode, currentNode) = tau(nextNode, currentNode)  + 1./ colony.ant(i).fitness;
+
+    end
+end
+
+end
