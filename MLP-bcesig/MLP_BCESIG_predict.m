cf5e276cb9381e233a8c7b74bad43b78815a7f10
martinaspeciale <mspeciale127001@gmail.com>
2025-09-19 10:03:31 +0200

    feat: add prediction helper using argmax over per-class sigmoid scores

---
+++ MLP-bcesig/MLP_BCESIG_predict.m
@@ -0,0 +1,13 @@
+function [y_pred] = MLP_BCESIG_predict(X, W1, W2)
+% Predict class labels for all the observations in X, 
+% where each row of X is a distinct observation.
+
+[rA2,A1,A0,rZ1] = MLP_BCESIG_forward(X, W1, W2);
+[~, argmax] = max(rA2);
+y_pred = argmax - 1;     %1:number_of_classes is mapped into 0:(number_of_classes-1)
+
+end
+
+
+
+
